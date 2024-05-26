ecde6f75dbbb32f24636d784c9accde7406136f0
Javed Akhtar <jved004@gmail.com>
2024-05-26 14:09:08 +0530

    added origin

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const name = 'Javed AKhtar';
 function hey() {
     console.log("Heyy!");
 }
+
+// when 2 people collaborate, they push and pull the code from a central server
+// github is a free gitserver
+// gits are commits, github is the source where git is pushed and pulled
+// central server is called origin conventionally
+
+// command = use git remote add origin <repository path>
+// check origin = git remote origin -v
